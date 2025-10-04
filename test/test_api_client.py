83bd798a6b5392658be016fb6f7a6d3d0fd7e84a
SelvitaPR <selva.pla.roj@gmail.com>
2025-10-04 22:43:40 +0100

    pseudocode tests for api client

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -1,2 +1,7 @@
 #import pytest
 #import mock
+
+#test search_term and date_from correct results
+#test results is a dictionary/JSON
+#test result have correct fields: pub date/title/URL
+#lenght of response? filtering works? 10 most recent
