ce244a1d2e453e9223ddbf89087c069a5ef26b72
SelvitaPR <selva.pla.roj@gmail.com>
2025-10-11 12:50:36 +0100

    create lambda_handler()

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,3 +1,8 @@
 #entry point for lambda
 #Responsibility: Same orchestration as cli.py, but triggered by AWS.
 #Keep it thin — it should just parse the Lambda event, call into your existing functions, and return a response.
+
+import boto3
+
+def lambda_handler():
+    lambda_client = boto3.client('lambda')
