128655a56eba64a0150d3aa95cd6a1077debc19e
SelvitaPR <selva.pla.roj@gmail.com>
2025-10-17 15:05:37 +0100

    update local logic

--- src/cli.py
+++ src/cli.py
@@ -4,7 +4,7 @@ import json
 from dotenv import load_dotenv
 
 from src.api_client import fetch_guardian_content
-from src.publisher import KinesisPublisher
+from src.publisher import LocalPublisher
 from src.utils import build_search_params, process_and_print_results
 
 # --- LOCAL CREDENTIAL LOADING ---
@@ -29,10 +29,9 @@ parser.add_argument('--date_from', help="date you'd like to search articles from
 if __name__ == '__main__':
     from datetime import date, datetime
 
-    # 1. Parse Arguments
     args = parser.parse_args()
     
-    # 2. Convert Arguments to Criteria (Handling Optional Date)
+    # Convert Arguments to Criteria (Handling Optional Date)
     if args.date_from is None or args.date_from.strip() == "":
         date_obj = date.today() 
         date_used_str = "today"  
@@ -49,30 +48,28 @@ if __name__ == '__main__':
         print("\nError: The --search term is mandatory. Please provide a query.")
         exit(1)
 
-    # 3. Create the dictionary for build_search_params
+    # Create the dictionary for build_search_params
     user_criteria = {
         'search_term': args.search,
         'date_from': date_obj
     }
     
-    # 4. Format Parameters for API
+    # Format Parameters for API
     api_params = build_search_params(user_criteria)
 
-    # 5. Fetch Content
+    # Fetch Content
     print(f"--- Searching Guardian for '{user_criteria['search_term']}' from {date_used_str} ---")
     data = fetch_guardian_content(API_URL_LOCAL, api_params, API_KEY_LOCAL)
 
-    # 6. Process, Print, and Publish Results
+    # Process, Print, and Publish Results
     if data:
         records_to_publish = data['response'].get('results', [])
         
-        # INITIALIZE PUBLISHER: Create the Kinesis client connection
-        publisher = KinesisPublisher(
+        publisher = LocalPublisher(
             stream_name=KINESIS_STREAM_NAME, 
             region_name=KINESIS_REGION
         )
         
-        # PUBLISH: Send the records to Kinesis
         publisher.publish(records_to_publish)
 
         # Print locally for confirmation
